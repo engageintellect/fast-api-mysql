aee415d39a3cbb82cff2d7a4920fbb989b026539
jc9361 <jc9361@gmail.com>
2022-10-30 23:08:50 -0700

    adding feedback for database connection status

--- config/db.py
+++ config/db.py
@@ -3,3 +3,12 @@ from sqlalchemy import create_engine, MetaData
 engine = create_engine('mysql+pymysql://root@localhost:3306/py_crud')
 meta=MetaData()
 con=engine.connect()
+
+if con:
+	print('-------------------------------')
+	print('Database connection successful.')
+	print('-------------------------------')
+else:
+	print('-------------------------------')
+	print('Database connection failed.')
+	print('-------------------------------')
